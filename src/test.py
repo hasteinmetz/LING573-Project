ae52422acc4db115e73c5691f953757a0101f910
Avani Pai <avani@iMac.local>
2022-04-16 20:04:04 -0700

    committing conda env file and code to test that environment is working as expected

---
+++ src/test.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import torch
+from transformers import RobertaTokenizer, RobertaForMaskedLM
+
+tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
+model = RobertaForMaskedLM.from_pretrained('roberta-base')
+text = "The capital of Japan is <mask>."
+inputs = tokenizer(text, return_tensors='pt')
+with torch.no_grad():
+	logits = model(**inputs).logits
+
+mask_token_index = (inputs.input_ids == tokenizer.mask_token_id)[0].nonzero(as_tuple=True)[0]
+
+predicted_token_id = logits[0, mask_token_index].argmax(axis=-1)
+print(tokenizer.decode(predicted_token_id))
