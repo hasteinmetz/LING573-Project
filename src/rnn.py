f2479a0c8098121df717b18553eac58a55f03402
hasteinmetz <hillel.steinmetz1@gmail.com>
2022-04-19 10:18:27 -0400

    working on rnn

--- src/rnn.py
+++ src/rnn.py
@@ -2,6 +2,7 @@
 
 from torch import cat, zeros, tensor
 from torch import nn
+from torch.utils import DataLoader
 import numpy as np
 
 def numpy_to_tensor(*args: np.ndarray):
@@ -18,34 +19,9 @@ def set_classes(categories):
         values.append(i)
     return classes, values
 
-# class Transformer(nn.Transformer):
-#     '''Class to define a BERT-style transformer.
-#     See: https://pytorch.org/docs/stable/generated/torch.nn.Transformer.html#torch.nn.Transformer
-#     Parameter defaults are based on BERT tiny (https://github.com/google-research/bert)'''
-#     def __init__(self, 
-#             d_model: int = 128, 
-#             nhead: int = 2, 
-#             num_encoder_layers: int = 6, 
-#             num_decoder_layers: int = 6, 
-#             dim_feedforward: int = 2048, 
-#             dropout: float = 0.1, 
-#             activation: Optional[Any] = "relu", 
-#             custom_encoder: Optional[Any] = None, 
-#             custom_decoder: Optional[Any] = None, 
-#             layer_norm_eps: float = 0.00001, 
-#             batch_first: bool = False, 
-#             norm_first: bool = False, 
-#             device=None, 
-#             dtype=None) -> None:
-#         super().__init__(d_model, nhead, num_encoder_layers, num_decoder_layers, 
-#             dim_feedforward, dropout, activation, custom_encoder, custom_decoder, 
-#             layer_norm_eps, batch_first, norm_first, device, dtype)
-
-# def train_transformer():
-#     return
 
 class RNN(nn.Module):
-    def __init__(self, embeddings: dict, hidden_layer_size, 
+    def __init__(self, embeddings: dict, hidden_layer_size, loss_fn,
             input_size, output_size, optimizer, learning_rate) -> None:
         '''Create a two-layer RNN model and define it's forward function'''
         super(RNN).__init__()
@@ -60,9 +36,9 @@ class RNN(nn.Module):
         '''Set the output layer function (if 2 use sigmoid)'''
         self.softmax = nn.LogSoftmax(dim=1)
         self.optimizer = optimizer(self.parameters(), lr=learning_rate)
+        self.loss_fn = loss_fn()
 
-    def forward(self, input: str, hidden):
-        input_vector = self.embeddings[input]
+    def forward(self, input_vector, hidden):
         combined_vector = cat((input_vector, hidden), 1)
         hidden = self.input_to_hidden(combined_vector)
         output = self.input_to_output(combined_vector)
@@ -74,7 +50,7 @@ class RNN(nn.Module):
 
 # want to input a tensor of size: word_embedding x sentence size
 
-def train_rnn(model: RNN, input_tensor, true_label):
+def train_rnn(model: RNN, batch, true_labels):
     '''Instructions on training and updating a model'''
     # initialize the hidden layer
     hidden = model.init_hidden_layer()
@@ -82,24 +58,35 @@ def train_rnn(model: RNN, input_tensor, true_label):
     # initialize the gradient calculation
     model.zero_grad()
 
-    # do you want to do like a "for batch" or something?
-    # TODO: CREATE TENSORS OF THE BATCHES OF DATA
-
     # get the result for the input tensor
-    for i in range(0, input_tensor[0].size()):
-        output, hidden = model.forward(input_tensor[i], hidden)
+    model_outputs = []
+    for input_tensor in batch:
+        for i in range(0, input_tensor[0].size()):
+            output, hidden = model.forward(input_tensor[i], hidden)
+        model_outputs.append(output)
 
-    loss_function = nn.NLLLoss()
-
-    loss = loss_function(output, true_label)
+    loss = model.loss_fn(model_outputs, true_labels)
 
     loss.backwards()
 
     model.optimizer.step()
 
-    return output, loss.item()
+    return model_outputs, loss.item()
 
 def training_iterations(model: RNN, dataset: List[str],
-    epochs: int = 1, learning_rate: int = 0.005):
+    epochs: int = 1, batch_size: int = 1, learning_rate: int = 0.005) -> None:
     iterations = len(dataset) * epochs
-    for 
+    data = DataLoader(dataset, batch_size)
+    for batch, (X, y) in enumerate(data):
+        output, loss = train_rnn(model, X, y)
+    
+        # print progress to stdout
+        if batch % 50 == 0:
+            loss, current_iter = loss.item(), batch * len(X)
+            print(f"loss: {loss:>7f}  [{current_iter:>5d}/{batch_size:>5d}]")
+            correct = (output.argmax(1)==y).type(torch.float).sum().item()
+            print(f"correct: {correct}"
+
+def main():
+    
+    
