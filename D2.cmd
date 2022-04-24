8f9cf67511c3fafbde4d431e7fa9a17995fed69e
hasteinmetz <hsteinm@uw.edu>
2022-04-24 08:53:10 -0700

    working on patas

---
+++ D2.cmd
@@ -0,0 +1,10 @@
+executable 		  = src/run.sh 
+getenv     		  = true
+arguments		  = 
+transfer_output_files     = outputs/baseline_output.csv
+output			  = outputs/condor.out
+error      		  = outputs/condor.error
+log        		  = outputs/condor.log
+transfer_executable 	  = false
+request_memory 		  = 2*1024
+queue
--- src/run.sh
+++ src/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/Users/hillel/Documents/UW/sp2022/systems/repo/env/bin/python /Users/hillel/Documents/UW/sp2022/systems/repo/src/baseline.py --train_sentences src/data/hahackathon_prepo1_train.csv --dev_sentences src/data/hahackathon_prepo1_dev.csv --hidden_layer 100 --learning_rate 0.0001 --batch_size 100 --epochs 64 --random_seeds src/data/random_seeds_100.txt --output_file outputs/baseline_output.csv
+python src/baseline.py --train_sentences src/data/hahackathon_prepo1_train.csv --dev_sentences src/data/hahackathon_prepo1_dev.csv --hidden_layer 100 --learning_rate 0.0001 --batch_size 100 --epochs 64 --random_seeds src/data/random_seeds_100.txt --output_file outputs/baseline_output.csv
