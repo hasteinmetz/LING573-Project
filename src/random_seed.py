8626acf54a31c94e4cb6ff662315873b19fa77b6
hasteinmetz <hillel.steinmetz1@gmail.com>
2022-04-24 14:08:12 -0400

    working locally

--- src/random_seed.py
+++ src/random_seed.py
@@ -4,10 +4,10 @@ Create and set random seeds to be used by the Python Dataloader
 
 import numpy as np
 
-def write_random_seed(no_epochs=100):
+def write_random_seed(filename, no_epochs=100):
     '''Get a random array of numbers to use as random seeds'''
     random_arr = np.random.randint(low=0, high=10000, size=(no_epochs+1,))
-    np.savetxt(f'random_seeds_{no_epochs}.txt', random_arr)
+    np.savetxt(f'{filename}_{no_epochs}.txt', random_arr)
     return random_arr
 
 def load_random_seed(filname, no_epochs=100):
@@ -19,7 +19,7 @@ def load_random_seed(filname, no_epochs=100):
         seedfile.close()
         return seeds
     except Exception:
-        random_arr = write_random_seed(no_epochs)
+        random_arr = write_random_seed(filname, no_epochs)
         return random_arr
 
 if __name__ == '__main__':
