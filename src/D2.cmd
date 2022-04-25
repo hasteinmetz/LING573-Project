e8ab0c8a38b3d4ee7b7c1b2d9378e1d60a14d9aa
Avani Pai <avanipai695@gmail.com>
2022-04-24 19:00:31 -0700

    d2 deliverable-ready outputs and bash scripts

--- src/D2.cmd
+++ src/D2.cmd
@@ -1,10 +1,11 @@
-executable 		  = src/run.sh 
+executable 		  = run.sh 
 getenv     		  = true
 arguments		  = 
 transfer_output_files     = outputs/baseline_output.csv
 output			  = outputs/condor.out
 error      		  = outputs/condor.error
 log        		  = outputs/condor.log
+request_GPUs              = 1
 transfer_executable 	  = false
 request_memory 		  = 2*1024
 queue
