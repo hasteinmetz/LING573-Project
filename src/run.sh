a888c3f3bf1f070e7853ffa1f3e98c3bdc3b5258
patas <avanipai695@gmail.com>
2022-04-28 11:59:11 -0700

    changed run.sh to use training data

--- src/run.sh
+++ src/run.sh
@@ -6,4 +6,4 @@ conda activate /projects/assigned/2122_ling573_elibales/env/
 
 cd .
 
-python src/baseline.py --train_sentences src/data/hahackathon_prepo1_dev.csv --dev_sentences src/data/hahackathon_prepo1_dev.csv --hidden_layer 100 --learning_rate 0.0001 --batch_size 64 --epochs 1 --output_file outputs/D2/d2_out.txt | tail -2 > results/D2_scores.out
+python src/baseline.py --train_sentences src/data/hahackathon_prepo1_train.csv --dev_sentences src/data/hahackathon_prepo1_dev.csv --hidden_layer 100 --learning_rate 0.0001 --batch_size 64 --epochs 1 --output_file outputs/D2/d2_out.txt | tail -2 > results/D2_scores.out
