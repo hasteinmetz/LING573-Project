9e56655fe017ddffbab882c154404dd5d7295390
Hillel (Hilly) Steinmetz <63203563+hasteinmetz@users.noreply.github.com>
2022-05-02 21:30:58 -0700

    added comment on "debugging" with condor

--- fine-tune-test.cmd
+++ fine-tune-test.cmd
@@ -1,8 +1,8 @@
 executable 		            = src/fine-tune.sh 
 getenv     		            = true
-arguments		            = "--debug 1"
-transfer_output_files       = outputs/test/fine-tune_results.csv, src/data/roberta-misclassified-examples.csv, src/models/roberta-fine-tuned
-output			            = outputs/test/ft.out
+arguments		              =         # type "--debug 1" here to run the model on fewer training and testing samples
+transfer_output_files     = outputs/test/fine-tune_results.csv, src/data/roberta-misclassified-examples.csv, src/models/roberta-fine-tuned
+output			              = outputs/test/ft.out
 error      		            = outputs/test/ft.error
 log        		            = outputs/test/ft.log
 request_GPUs                = 1
