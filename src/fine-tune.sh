3cad3937c9ed117ad2b0a336d92adf1019ab3d67
patas <hsteinm@patas.ling.washington.edu>
2022-05-03 19:25:05 -0700

    refactored code

--- src/fine-tune.sh
+++ src/fine-tune.sh
@@ -3,4 +3,4 @@
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate 573-project
 
-python src/fine-tune.py --train_sentences src/data/hahackathon_prepo1_train.csv --dev_sentences src/data/hahackathon_prepo1_dev.csv --learning_rate 5e-5 --batch_size 64 --epochs 1 --output_file outputs/test/fine-tune_results.csv --save_file src/models/roberta-fine-tuned "$@"
+python src/fine-tune.py --train_sentences src/data/hahackathon_prepo1_train.csv --dev_sentences src/data/hahackathon_prepo1_dev.csv --learning_rate 5e-5 --batch_size 32 --epochs 1 --output_file outputs/test/fine-tune_results.csv --save_file src/models/roberta-fine-tuned "$@"
--- src/write_env.sh
+++ src/write_env.sh
@@ -1,3 +1,4 @@
+conda activate 573-project
 conda env export --no-builds --from-history > $1 
 NEWFILE=$( head -n $(expr $(cat $1 | wc -l) - 1) $1 )
 echo "$NEWFILE" > $1
