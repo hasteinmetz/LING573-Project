6710b076ec4dfb953ed1b3a84d39bc7b28519306
elibales <103016069+elibales@users.noreply.github.com>
2022-05-07 14:19:43 -0700

    returns numpy array of [num_jokes, NER_categories]

--- src/named_entity_recognizer.py
+++ src/named_entity_recognizer.py
@@ -1,22 +1,22 @@
 import nltk
 import csv
-import sys
+import numpy as np
+
+# call with get_ner_matrix(path/to/input_file)
 
 
-def write_to_file(output_file, named_entity_list):
-    with open(output_file, "w", newline='', encoding="utf-8") as my_csv:  # create training data file
-        my_writer = csv.writer(my_csv)
-        for named_entity in named_entity_list:
-            my_writer.writerow(named_entity)
-    my_csv.close()
-
-
-def main():
-    input_file = sys.argv[1]
-    output_file = sys.argv[2]
-    correct_label_column = int(sys.argv[3])
-    ne_groupings = []
-    # print("here!")
+def get_ner_matrix(input_file):  # returns a numpy array of [num_documents, 6] where 6 is the number of NER categories
+    input_file = input_file
+    label_list = ["GPE", "PERSON", "ORGANIZATION", "FACILITY", "LOCATION", "GSP"]  # hardcoded to ensure length
+    label_dict = {
+        "GPE": 0,
+        "PERSON": 1,
+        "ORGANIZATION": 2,
+        "FACILITY": 3,
+        "LOCATION": 4,
+        "GSP": 5
+    }
+    total_list = []
     with open(input_file, encoding='utf-8') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=",")
         for row in csv_reader:
@@ -25,28 +25,20 @@ def main():
             ne_tree = nltk.ne_chunk(tagged)  # develop tree, which defines what is a named entity
 
             ne_list = []
+
             for item in ne_tree:
                 if not isinstance(item, tuple):  # non-Named-entities are saved as tuples, others are Named entities
                     ne_list.append(item)  # append Named entities
 
-            if len(ne_list) > 0:
-                temp = []
-                for item in ne_list:
-                    group = []
-                    # print(item)
-                    for j in range(len(item)):
-                        group.append(item[j][0])  # get the object recognized as NE
-                    temp.append(" ".join(group))
-                temp.append(int(row[correct_label_column]))  # correct_label, CHANGE DEPENDING ON INPUT FILE!
-                ne_groupings.append(temp)
-            else:
-                temp = []
-                temp.append("")
-                temp.append(int(row[correct_label_column]))
-                ne_groupings.append(temp)
+            zerod = np.zeros(len(label_list))
+            for item in ne_list:
+                label = item.label()
+                index = label_dict.get(label)
+                if index is not None:
+                    zerod[index] += 1
+            total_list.append(zerod)
 
+    ner_array = np.asarray(total_list)
     csv_file.close()
-    write_to_file(output_file, ne_groupings)
 
-
-main()
+    return ner_array
