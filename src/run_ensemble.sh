354e7fab68f369275f1b0e38d18e5b7a0d0d93a5
hasteinmetz <hsteinm@uw.edu>
2022-05-09 14:01:39 -0700

    end-to-end ensemble, but issues with training roberta

--- src/run_ensemble.sh
+++ src/run_ensemble.sh
@@ -3,4 +3,4 @@
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate 573-project
 
-python src/ensemble.py --random_forest_config src/configs/random_forest.json --logistic_regression_config src/configs/logistic_regression.json --train_data_path src/data/hahackathon_prepo1_train.csv --dev_data_path src/data/hahackathon_prepo1_dev.csv --output_file outputs/ensemble/d3_out.txt 
+python src/ensemble.py --train_data_path src/data/hahackathon_prepo1_train.csv --dev_data_path src/data/hahackathon_prepo1_dev.csv --output_file outputs/D3/ensemble/basic_test.out --debug 1
