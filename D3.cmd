682cbcac429c614944bf6f589efc6744c33eed07
hasteinmetz <hsteinm@uw.edu>
2022-05-08 19:27:55 -0700

    saving progress

--- D3.cmd
+++ D3.cmd
@@ -1,10 +1,10 @@
 executable           = src/run_ensemble.sh 
 getenv               = true
 arguments            = 
-transfer_output_file = outputs/ensemble/d3_out.txt, log
-output               = outputs/condor.out
-error                = outputs/condor.error
-log                  = outputs/condor.log
+transfer_output_files = outputs/D3/ensemble/d3_out.txt, log
+output               = outputs/D3/ensemble/condor.out
+error                = outputs/D3/ensemble/condor.error
+log                  = outputs/D3/ensemble/condor.log
 request_GPUs         = 1
 transfer_executable  = false
 request_memory       = 2*1024
