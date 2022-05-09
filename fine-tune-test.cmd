5618ab67a308f8ee60bd4e2609e06fbec168035f
patas <avanipai695@gmail.com>
2022-05-08 20:08:32 -0700

    cleaning up messy merge

--- fine-tune-test.cmd
+++ fine-tune-test.cmd
@@ -1,14 +1,14 @@
-executable 		            = src/fine-tune-$(model).sh 
+executable 		            = src/fine-tune.sh 
 getenv     		            = true
 # type "--debug 1" in arguments to run the model on fewer training and testing samples
 # type "--model_folder {path}" to load the model at the path specified
 arguments		            = 
-transfer_output_files       = outputs/D3/$(model)/fine-tune_results.csv, src/data/$(model)-misclassified-examples.csv, src/models/$(model)-fine-tuned
-output			            = outputs/D3/$(model)/ft.out
-error      		            = outputs/D3/$(model)/ft.error
-log        		            = outputs/D3/$(model)/ft.log
+transfer_output_files       = outputs/D3/roberta/fine-tune_results.csv, src/data/roberta-misclassified-examples.csv, src/models/roberta-fine-tuned
+output			            = outputs/D3/roberta/ft.out
+error      		            = outputs/D3/roberta/ft.error
+log        		            = outputs/D3/roberta/ft.log
 request_GPUs                = 1
 transfer_executable 	    = false
 request_memory 		        = 2*1024
 notification                = complete
-queue model from batch.txt
+queue 
