f616dd6b0beaaae9d8f4f191b353ed0b7c92e4a4
patas <hsteinm@patas.ling.washington.edu>
2022-05-16 19:56:26 -0700

    saving work on D4

--- src/configs/create_env.sh
+++
@@ -1,3 +0,0 @@
-conda env create --force -f environment.yml --prefix /projects/assigned/2122_ling573_elibales/env
-conda activate /projects/assigned/2122_ling573_elibales/env
-python3 -m spacy download en_core_web_sm
