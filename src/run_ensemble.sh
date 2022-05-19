e79d231eccce5f7f3f82a8eb837f69a1e9d62db4
patas <hsteinm@patas.ling.washington.edu>
2022-05-18 17:01:16 -0700

    debuggin

--- src/run_ensemble.sh
+++ src/run_ensemble.sh
@@ -3,4 +3,4 @@
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate 573-project
 
-python src/ensemble.py --train_data_path src/data/hahackathon_prepo1_train.csv --dev_data_path src/data/hahackathon_prepo1_dev.csv --output_file outputs/D4/ensemble-test/ensemble.csv
+python src/ensemble_hilly.py --train_data_path src/data/hahackathon_prepo1_train.csv --dev_data_path src/data/hahackathon_prepo1_dev.csv --output_file outputs/D4/ensemble-test/ensemble.csv "$@"
