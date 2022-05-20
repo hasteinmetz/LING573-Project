50eec66ef0360f97136ce13de601f42562d47a1d
patas <hsteinm@patas.ling.washington.edu>
2022-05-19 20:48:33 -0700

    trying new pretraining strategy

--- src/executables/run_ensemble.sh
+++ src/executables/run_ensemble.sh
@@ -3,4 +3,4 @@
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate /projects/assigned/2122_ling573_elibales/env/
 
-python src/ensemble_hilly.py --train_data_path src/data/D4_hahackathon_prepo1_train.csv --dev_data_path src/data/D4_hahackathon_prepo1_dev.csv --output_file outputs/D4/ensemble-test/ensemble-controversy.csv --hurtlex_path src/data/hurtlex_en.tsv
+python src/ensemble_hilly.py --train_data_path src/data/D4_hahackathon_prepo1_train.csv --dev_data_path src/data/D4_hahackathon_prepo1_dev.csv --hurtlex_path src/data/hurtlex_en.tsv "$@"
