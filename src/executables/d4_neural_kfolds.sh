3c5d510a34e2764c968ac0fa1bf66cfa3b20d3f0
hasteinmetz <hsteinm@uw.edu>
2022-05-26 16:33:39 -0700

    made small changes; runnign expts; reorganized repo

--- src/executables/d4_neural_kfolds.sh
+++ src/executables/d4_neural_kfolds.sh
@@ -3,4 +3,4 @@
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate /projects/assigned/2122_ling573_elibales/env/
 
-time python src/ensemble_hilly_kfolds.py --train_data_path src/data/D4_hahackathon_prepo1_train.csv --dev_data_path src/data/D4_hahackathon_prepo1_dev.csv --hurtlex_path src/data/hurtlex_en.tsv --error_path src/data/nn-kfolds-misclassified --model_save_path src/models/nn_ensemble_kfolds --output_path outputs/D4/nn_ensemble_kfolds "$@"
+time python src/neural_ensemble_kfolds.py --train_data_path src/data/D4_hahackathon_prepo1_train.csv --hurtlex_path src/data/hurtlex_en.tsv --error_path src/data/nn-kfolds-misclassified --model_save_path src/models/nn_ensemble_kfolds --output_path outputs/D4/nn_ensemble_kfolds "$@"
