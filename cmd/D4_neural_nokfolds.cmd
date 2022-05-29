a219e4d52d33334c8d2b76a8e028be8472a466b6
hasteinmetz <hsteinm@uw.edu>
2022-05-29 10:35:16 -0700

    ran on test data

--- cmd/D4_neural_nokfolds.cmd
+++ cmd/D4_neural_nokfolds.cmd
@@ -10,8 +10,8 @@ transfer_executable   = false
 request_memory        = 3*1024
 stream_output         = True
 queue job, dim_red, arguments from (
-    humor, pca, "--index 1 --job $(job) --dim_reduc_method $(dim_red)"
-    controversy, pca, "--index 2 --job $(job) --dim_reduc_method $(dim_red)"
-    humor, kbest, "--index 1 --job $(job) --dim_reduc_method $(dim_red)"
-    controversy, kbest, "--index 2 --job $(job) --dim_reduc_method $(dim_red)"
+    humor, pca, "--index 1 --job $(job) --dim_reduc_method $(dim_red) --test test"
+    controversy, pca, "--index 2 --job $(job) --dim_reduc_method $(dim_red) --test test"
+    humor, kbest, "--index 1 --job $(job) --dim_reduc_method $(dim_red) --test test"
+    controversy, kbest, "--index 2 --job $(job) --dim_reduc_method $(dim_red) --test test"
 )
